947b9132ffc04daa5e5060408b8a2f2ea7b46132
VladaNills <vladarub@yandex.ru>
2019-03-31 00:23:26 +0300

    auto filling email authorization

--- app/src/main/java/com/example/vlada_rubezhanskaya_pr/screen/LoginActivity.kt
+++ app/src/main/java/com/example/vlada_rubezhanskaya_pr/screen/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.example.vlada_rubezhanskaya_pr.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.email
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
@@ -14,31 +15,40 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        login_login.onClick {
-            login_user()
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            val email = auth.currentUser!!.email
+            editText_email.setText(email)
+//            val intent = Intent(this@LoginActivity, CategoryActivity::class.java)
+//            startActivity(intent)
+//            finish()
         }
+    login_login.onClick()
+    {
+        login_user()
     }
+}
 
-    private fun login_user() {
-        val email = editText_email.text.toString()
-        val password = editText_password.text.toString()
+private fun login_user() {
+    val email = editText_email.text.toString()
+    val password = editText_password.text.toString()
 //            Log.d("LoginActivity", "email is " + email)
 //            Log.d("LoginActivity", "password is " + password)
-        if (email.isEmpty() || (password.isEmpty())) {
-            longToast(getString(R.string.message_type_password))
-            return
-        }
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener {
-                if (!it.isSuccessful)
-                    return@addOnCompleteListener
-                val intent = Intent(this@LoginActivity, CategoryActivity::class.java)
-                startActivity(intent)
-
-            }
-            .addOnFailureListener {
-                longToast(getString(R.string.message_type_correct_data))
-            }
-
+    if (email.isEmpty() || (password.isEmpty())) {
+        longToast(getString(R.string.message_type_password))
+        return
     }
+    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener {
+            if (!it.isSuccessful)
+                return@addOnCompleteListener
+            val intent = Intent(this@LoginActivity, CategoryActivity::class.java)
+            startActivity(intent)
+
+        }
+        .addOnFailureListener {
+            longToast(getString(R.string.message_type_correct_data))
+        }
+
+}
 }
